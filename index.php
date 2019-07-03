9f6514e746be122b3da70f622ce4c0310c274904
lyubomyr83 <lyubomyr83@mail.ru>
2019-07-03 18:40:54 +0300

    Pifagor table

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
+require_once "lib/functions.php";
 require_once "header.php";
 ?>
 <h1>Главная страница</h1>
 <?php
 
 require_once "menu.php";
+?>
+<br><br><br><br>
+<form method="post">
+    Введите число строк
+    <input type="text" name="str">
+    Введите число столбцов
+    <input type="text" name="stlb">
+    <input type="submit" value="Нарисовать таблицу">
+</form>
+<?php
+pifagor($_POST['str'],$_POST['stlb']);
--- lib/functions.php
+++ lib/functions.php
@@ -14,6 +14,25 @@ function hello($name)
 }
 
 
+function pifagor($str,$stlb)
+{
+
+    echo "\n\n<table>";
+    for ($i=1;$i<=$str;$i++)
+    {
+
+        echo "\n\t<tr>";
+            for ($k=1;$k<=$stlb;$k++)
+            {
+                $td = ($i==1)?$k:$i*$k;
+                echo "\n\t\t<td>$td</td>";
+            }
+        echo "\n\t</tr>";
+    }
+
+    echo "\n</table>";
+}
+
 
 
 
